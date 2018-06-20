d0c2a22c71259399309a2a94ac40bb1fb4d04504
Jen Shannon <jennifer.shannon@galvanize.com>
2018-06-20 16:32:53 -0400

    Add more changes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const environment = process.env.NODE_ENV || 'development';
+const knexConfig = require('../knexfile.js')[environment];
+const knex = require('knex')(knexConfig);
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  knex('users')
+  .then(function(users){
+    res.render('users', {
+      users
+    });
+  })
 });
 
 module.exports = router;
